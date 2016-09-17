bf8859493ca7eaf63f67094deb064da61eb1da5b
Marcus <marcusa3.14@gmail.com>
2016-09-17 15:59:15 -0400

    js

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 var myFirebaseRef = new Firebase("https://maple-6845a.firebaseio.com/");
 for(var n 0; n<5; n++)
 {
-	myFirebaseRef.child("worldId/0Data/Table/").on("value", function(snapshot) {
-		for(var x = 0; x<10; x++)
+	myFirebaseRef.child("worldId/"+n+"Data/Table/").on("value", function(snapshot) {
+		for(var x = 0; x<2; x++)
 		{
 			var time = snapshot.val()[x][0];
 			var image = snapshot.val()[x][1];
 			var name = snapshot.val()[x][2];
 			var price = snapshot.val()[x][3];
 			var world = snapshot.val()[x][4];
+
 			var table = document.getElementById("table");
-			var row = insertRow
-			document.createElement("TR" "<td><img src='"+image+"' height:35px width:35px>"+name+"</td>     <td>"+price+"</td>      <td>"+world+"</td>      <td>"+time+"</td>      <td><span onclick='this.parentElement.parentElement.style.display='none'' class='w3-closebtn w3-padding w3-margin-right w3-medium'>X</span></td>"
+			var row = table.insertRow(0);
+			var c0 = row.insertCell(0);
+			c0.innerHTML = "<img src='"+image+"' height:35px width:35px>"+name;
+			var c1 = row.insertCell(1);
+			c1.innerHTML = price+"";
+			var c2 = row.insertCell(2);
+			c2.innerHTML = world+"";
+			var c3 = row.insertCell(3);
+			c3.innerHTML = time+"";
+			var c4 = row.insertCell(4);
+			c4.innerHTML = "<span onclick='this.parentElement.parentElement.style.display='none'' class='w3-closebtn w3-padding w3-margin-right w3-medium'>X</span>"
 		}
 	});
 }
