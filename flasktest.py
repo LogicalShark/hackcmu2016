bcffe725b6d64f75f38a8fc204e4713b34e022d6
Namita Dongre <ndongre898@gmail.com>
2016-09-17 01:18:35 -0400

    f

--- flasktest.py
+++ flasktest.py
@@ -2,16 +2,19 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index(): pass
 
-@app.route('/projects/')
-def projects():
-    return 'The project page'
+@app.route('/login')
+def login(): pass
 
-@app.route('/about')
-def about():
-    return 'The about page'
+@app.route('/user/<username>')
+def profile(username): pass
+
+with app.test_request_context():
+	print url_for('index')
+	print url_for('login')
+	print url_for('login', next='/')
+	print url_for('profile', username='John Doe')
 
 if __name__ == "__main__":
 	app.run()
