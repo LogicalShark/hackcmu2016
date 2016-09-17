7da756a79ce107e17c949e01375d0f6b0a9831c9
Marcus <marcusa3.14@gmail.com>
2016-09-17 16:11:36 -0400

    js

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ for(var n 0; n<5; n++)
 	myFirebaseRef.child("worldId/"+n+"Data/Table/").on("value", function(snapshot) {
 		for(var x = 0; x<2; x++)
 		{
-			var time = snapshot.val()[x][0];
-			var image = snapshot.val()[x][1];
+			var time = snapshot[x][0].val();
+			var image = snapshot[x][1].val();
 			var name = snapshot.val()[x][2];
 			var price = snapshot.val()[x][3];
 			var world = snapshot.val()[x][4];
@@ -24,6 +24,9 @@ for(var n 0; n<5; n++)
 			c4.innerHTML = "<span onclick='this.parentElement.parentElement.style.display='none'' class='w3-closebtn w3-padding w3-margin-right w3-medium'>X</span>"
 		}
 	});
+	myFirebaseRef.child("worldId/"+n+"Data/Stability/").on("value", function(snapshot) {
+	}
+
 }
 // var time;
 // var image;
