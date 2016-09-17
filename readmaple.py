ddbd27db90250143f58d190774e57707f30a892f
Hyung Ju Moon <hyungjumoon@gmail.com>
2016-09-16 23:10:47 -0400

    data is parsed into a list

--- readmaple.py
+++ readmaple.py
@@ -1,6 +1,14 @@
 from urllib.request import Request, urlopen
+from firebase import firebase
+import json
+
 
 req = Request('http://maplestory.io/api/fm/world/0/rooms', headers={'User-Agent': 'Mozilla/5.0'})
-webpage = urlopen(req).read()
+response = urlopen(req)
 
-print(webpage)
+#parsing?
+string = response.read().decode('utf-8')
+
+json_obj = json.loads(string)
+print(json_obj[1]['shops'][0]['items'][0]['isIdentified']) # prints the string with 'source_name' key
+print(type(json_obj))
